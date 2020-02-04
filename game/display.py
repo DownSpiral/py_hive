8450ae2e24c512f15eb2c93b8402987c4a56758d
brentwishart <bwishart@teachtci.com>
2020-02-04 15:15:58 -0800

    extra

--- game/display.py
+++ game/display.py
@@ -12,21 +12,6 @@ class Display:
 
         self.display = pygame.display.set_mode((self.width, self.height))
 
-    def render_board(self):
-        self.display.fill((255,255,255))
-        for row in self.game.board.tiles:
-            for tile in row:
-                self.render_tile(tile)
-
-    def render_tile(self, tile):
-        (x, y) = (tile.coord.x, tile.coord.y)
-        (rx, ry) = (self.ppt * x, self.ppt * y)
-
-        self.render_square(rx, ry, self.ppt, tile.color())
-
-    def render_square(self, x, y, length, color):
-        pygame.draw.rect(self.display, color, pygame.Rect(x, y, length, length))
-
     def render_updates(self, updates):
         # Figure out how to render updates
         pygame.display.flip()
