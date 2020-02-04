2fe48ed0282cb1a41c8e88e531609a288fdc714f
brent <brent.wish@gmail.com>
2020-02-03 16:15:00 -0800

    draw small red square

--- game/draw.py
+++ game/draw.py
@@ -1,12 +1,9 @@
 import pygame
 
-def tap_pygame():
-    if not pygame.get_init():
-        pygame.init()
-
 class Draw:
     def __init__(self, start=False):
-        tap_pygame()
+        if not pygame.get_init():
+            pygame.init()
 
         self.width = 800
         self.height = 600
@@ -14,19 +11,10 @@ class Draw:
         self.display = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
 
-    def handle_events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                print('quit')
-                pygame.quit()
-                quit()
-
-            if event.type == pygame.KEYUP:
-                print('up')
-
-            if event.type == pygame.KEYDOWN:
-                print('down')
-
     def update(self, speed=60):
         pygame.display.update()
         self.clock.tick(speed)
+
+    def square(self, s):
+        pygame.draw.rect(self.display, (255, 0, 0), pygame.Rect(1, 1, s, s))
+        pygame.display.update()
--- game/play_hive.py
+++ game/play_hive.py
@@ -1,3 +1,4 @@
+import pygame
 from draw import Draw
 
 GAME_SPEED = 60
@@ -5,7 +6,23 @@ GAME_SPEED = 60
 if __name__ == '__main__':
     draw = Draw()
 
-    gameExit = False
-    while not gameExit:
-        draw.handle_events()
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print('quit')
+                run = False
+
+            if event.type == pygame.KEYUP:
+                print('up')
+
+            if event.type == pygame.KEYDOWN:
+                draw.square(5)
+                print('down')
+
+
+
         draw.update(GAME_SPEED)
+        pygame.display.flip()
+
+    pygame.quit()
