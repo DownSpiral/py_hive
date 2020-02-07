527795fd74019184c0ec140e263172cdab504789
brentwishart <bwishart@teachtci.com>
2020-02-06 23:35:15 -0800

    Add Item and Food class

---
+++ common/food.py
@@ -0,0 +1,9 @@
+class Food(Item):
+    def __init__(self, **settings):
+        self.quantity = settings['quantity']
+        super.__init__(**settings)
+
+    def wat(self):
+        print(self.quantity)
+        print(self.tile.x, self.tile.y)
+        super().wat()
---
+++ common/item.py
@@ -0,0 +1,10 @@
+# Base class for all things that can be an item on a tile
+# Should all item types have a corresponding Action type?
+class Item:
+    def __init(self, tile, **settings):
+        self.tile = tile
+        self.settings = settings
+
+    def wat(self):
+        for k, v in self.settings:
+            print(f"{k} wat {v}")
